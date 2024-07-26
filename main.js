720a7c10652cd305a956bc238ff0f61d922f3422
Julia <julia.benachio@gmail.com>
2024-07-26 07:30:26 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const novaTarefa = $('#tarefa-adicionada').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<p>${novaTarefa}</p>`).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#tarefa-adicionada').val('');
+    })
+})
